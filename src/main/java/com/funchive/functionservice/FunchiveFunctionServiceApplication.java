e4c4a2c25bc5add0144da991e53236d5ebaf818c
nghialt3670 <nghialt3670@gmail.com>
2025-06-10 16:48:54 +0700

    Initial commit

---
+++ src/main/java/com/funchive/functionservice/FunchiveFunctionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.funchive.functionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunchiveFunctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunchiveFunctionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funchive/functionservice/FunchiveFunctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funchive.functionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunchiveFunctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
